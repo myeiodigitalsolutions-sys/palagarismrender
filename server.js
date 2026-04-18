9614a43cb7334de6ecb9d997a328772b02f0e8c3
myeiodigitalsolutions-sys <myeiodigitalsolutions@gmail.com>
2026-04-18 13:44:55 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:5000',
   'http://127.0.0.1:3000',
+  'https://palagarismrender.onrender.com',
   'https://plagiarism-checker-olive.vercel.app'
 ];
 
